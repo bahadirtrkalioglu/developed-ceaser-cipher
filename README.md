1c83675f2cbf417430973013b850cfc10801ace3
Bahadır Tarık Alioğlu <alioglub51@gmail.com>
2022-10-23 22:56:24 +0300

    Added README page and bugs fixed

---
+++ README.md
@@ -0,0 +1,28 @@
+# Super Caesar Cipher Encryption-Decryption Programme
+
+This program can encode and decode your text input using Caesar Cipher's encryption method.
+
+## How it works
+It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet.
+
+### What is shift number?
+Let's think an example, if our shift number is 3, A would be replaced by D, B would become E, and so on.
+
+### How to encode
+You should type 'encode' when it asks. Then, set a shift number (don't forget it if you want to decode it).
+It shows your outcome in the screen.
+
+### How to decode
+If you want to decode your text, you should type your encoded text and enter the correct shift number.
+
+## Extra Features
+In Caesar Cipher's encryption method it only encrypts the letters, but sometimes we want to encode symbols and numbers.
+In Super Caesar Cipher Encryption-Decryption you can encode and decode them too.
+By now you can encode and decode 28 most used symbols.
+
+#### 28 Most Used Symbols
+'#', '$', '%', '&', '/', " ' ", '(', ')', '*', '+', ',', '-', '.', ':', ';', "<", '>', '=', '?', '@', '[', ']', '_', '{', '}', "|", '!', '^'
+
+#### Numbers
+You can encode and decode numbers   
+'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-alphabet = ['a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g', 'ğ', 'h', 'ı', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'ö', 'p', 'q', 'r', 's', 'ş', 't', 'u','ü', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g', 'ğ', 'h', 'ı', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'ö', 'p', 'q', 'r', 's', 'ş', 't', 'u','ü', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['#', '$', '%', '&', '/', "'", '(', ')', '*', '+', ',', '-', '.', ':', ';', "<", '>', '=', '?', '@' '[', ']', '_', '{', '}', "|", '!', '^', '#', '$', '%', '&', '/', "'", '(', ')', '*', '+', ',', '-', '.', ':', ';', "<", '>', '=', '?', '@' '[', ']', '_', '{', '}', "|", '!', '^']
+symbols = ['#', '$', '%', '&', '/', "'", '(', ')', '*', '+', ',', '-', '.', ':', ';', "<", '>', '=', '?', '@', '[', ']', '_', '{', '}', "|", '!', '^', '#', '$', '%', '&', '/', "'", '(', ')', '*', '+', ',', '-', '.', ':', ';', "<", '>', '=', '?', '@', '[', ']', '_', '{', '}', "|", '!', '^']
 def caesar(start_text, shift_for_letters, shift_for_nums, shift_for_syms, cipher_direction):
   end_text = ""
   if cipher_direction == "decode":
@@ -26,25 +26,25 @@ def caesar(start_text, shift_for_letters, shift_for_nums, shift_for_syms, cipher
 
 from art import logo
 print(logo)
-print("....::::The Ceasar Chiper: Encryption Program::::....\n")
+print("....::::The Caesar Cipher: Encryption Program::::....\n")
 help = input("If you need help, type 'help', if not type 'no': ").lower()
 if help == "help":
-  print("\nThis program can encode and decode your text input using Ceasar Chiper's Encryption method. We need a shift number for shift the alphabet.\nWhat is shift number? For example, if our shift number is 3, A would be replaced by D, B would become E, and so on.\nAnd when you want to decode your text, you should type your correct shift number")
+  print("\nThis program can encode and decode your text input using Caesar Cipher's encryption method. We need a shift number for shift the alphabet.\nWhat is shift number? For example, if our shift number is 3, A would be replaced by D, B would become E, and so on.\nAnd when you want to decode your text, you should type your correct shift number")
 
 should_end = False
 while not should_end:
 
-  direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt:\n")
-  text = input("Type your message:\n").lower()
-  letter_shift = int(input("Type the shift number:\n"))
-  shift_lets = letter_shift % 32
+  direction = input("\n\nType 'encode' to encrypt, type 'decode' to decrypt: ")
+  text = input("Type your message: ").lower()
+  letter_shift = int(input("Type the shift number: "))
+  shift_lets = letter_shift % 26
   shift_nums = letter_shift % 10
-  shift_syms = letter_shift % 27 # modded for list range
+  shift_syms = letter_shift % 28 # modded for list range
 
   caesar(start_text=text, shift_for_letters=shift_lets, shift_for_nums=shift_nums, shift_for_syms=shift_syms, cipher_direction=direction)
 
-  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  restart = input("\tType 'yes' to do more. Otherwise type 'no' to exit the program: ")
   if restart == "no":
     should_end = True
-    print("Cipher Says Goodbye")
+    print("\n\nCipher Says Goodbye\n\n")
     
